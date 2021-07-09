0bbc7cd6a19dff4bc1993eeec40bb56a58525c45
Andreas Karis <ak.karis@gmail.com>
2021-07-09 06:33:13 +0200

    .

--- tools/checkovnroutes.sh
+++ tools/checkovnroutes.sh
@@ -5,7 +5,7 @@ for pod in $(oc get pods -n ovn-kubernetes -l name=ovnkube-master -o name); do
 	oc exec -n ovn-kubernetes -it -c ovn-northd $pod -- ovn-nbctl lr-list
 	routers=$(oc exec -n ovn-kubernetes -it -c ovn-northd $pod -- ovn-nbctl lr-list | awk '{print $1}')
 	for r in $routers ; do
-		echo "=== Routes for router: $f ==="
+		echo "=== Routes for router: $r ==="
 		oc exec -n ovn-kubernetes -it -c ovn-northd $pod -- ovn-nbctl lr-route-list $r
 	done
 done
