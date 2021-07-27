63bfe7c46d4f3fd83a0137dba22327fd1fa8bd1c
Andreas Karis <ak.karis@gmail.com>
2021-07-27 18:34:26 +0200

    Added script for OpenShift

--- tools/checkovnroutes-openshift.sh
+++ tools/checkovnroutes-openshift.sh
@@ -3,7 +3,7 @@
 for pod in $(oc get pods -n openshift-ovn-kubernetes -l app=ovnkube-master -o name); do
 	echo "Routers are:"
 	oc exec -n openshift-ovn-kubernetes -it -c ovn-northd $pod -- ovn-nbctl lr-list
-	routers=$(oc exec -n ovn-kubernetes -it -c ovn-northd $pod -- ovn-nbctl lr-list | awk '{print $1}')
+	routers=$(oc exec -n openshift-ovn-kubernetes -it -c ovn-northd $pod -- ovn-nbctl lr-list | awk '{print $1}')
 	for r in $routers ; do
 		echo "=== Routes for router: $r ==="
 		oc exec -n openshift-ovn-kubernetes -it -c ovn-northd $pod -- ovn-nbctl lr-route-list $r
