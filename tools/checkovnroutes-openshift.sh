183013bda288221959a0469244c20773f6f7e380
Andreas Karis <ak.karis@gmail.com>
2021-07-27 18:39:03 +0200

    Added script for OpenShift

--- tools/checkovnroutes-openshift.sh
+++ tools/checkovnroutes-openshift.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -x
 
 for pod in $(oc get pods -n openshift-ovn-kubernetes -l app=ovnkube-master -o name); do
-	echo "Routers are:"
-	oc exec -n openshift-ovn-kubernetes -it -c ovn-northd $pod -- ovn-nbctl lr-list
-	routers=$(oc exec -n openshift-ovn-kubernetes -it -c ovn-northd $pod -- ovn-nbctl lr-list | awk '{print $1}')
-	for r in $routers ; do
-		echo "=== Routes for router: $r ==="
-		oc exec -n openshift-ovn-kubernetes -it -c ovn-northd $pod -- ovn-nbctl lr-route-list $r
-	done
+	oc exec -n openshift-ovn-kubernetes -it -c northd $pod -- ovn-nbctl lr-list
+	if [ "$?" == "0" ]; then
+		echo "Routers are:"
+		oc exec -n openshift-ovn-kubernetes -it -c northd $pod -- ovn-nbctl lr-list
+		routers=$(oc exec -n openshift-ovn-kubernetes -it -c ovn-northd $pod -- ovn-nbctl lr-list | awk '{print $1}')
+		for r in $routers ; do
+	  		echo "=== Routes for router: $r ==="
+	  		oc exec -n openshift-ovn-kubernetes -it -c northd $pod -- ovn-nbctl lr-route-list $r
+		done
+	fi
 done
